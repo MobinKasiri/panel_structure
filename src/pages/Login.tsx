34b78abd70ea3ac36a2ee5d3c0576bcb1c344b0c
amin abdi <a.abdi@sibitec.ir>
2024-12-04 11:37:15 +0330

    login page added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,45 +1,74 @@
-import { Box, Button, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+} from "@mui/material";
 import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import apiCall from "../lib/services/apicall";
 import Cookies from "js-cookie";
 import { accessTokenExist, makeDateFromNow } from "../lib/utils/functions";
 import { Navigate, useNavigate } from "react-router-dom";
 import { LocalNames } from "../lib/constant/local_enums";
+import LoginImage from "../assets/login/image.webp";
+// import HoldingImage from "../assets/login/holding.svg";
+import { Input } from "design_system";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 
 interface IFormInput {
-  credential: string;
+  username: string;
   password: string;
 }
 
 interface LoginResponse {
-  token: string;
+  jwt: string;
 }
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const { control, handleSubmit } = useForm({
+  const { mutate } = useMutation<LoginResponse, Error, IFormInput>({
+    mutationFn: (data) =>
+      apiCall<LoginResponse>({
+        url: "/v1/user/auth",
+        data: {
+          appName: "holding",
+          ...data,
+        },
+        method: "post",
+      }),
+    onSuccess: (res) => {
+      const token = res.jwt;
+      if (rememberMe) {
+        Cookies.set(LocalNames.token, token, {
+          expires: makeDateFromNow(
+            import.meta.env.VITE_TOKEN_EXPIRATION_SECOND,
+          ),
+        });
+      } else {
+        sessionStorage.setItem(LocalNames.token, token);
+      }
+      navigate("/");
+    },
+  });
+
+  const [rememberMe, rememberMeSet] = useState(false);
+
+  const { handleSubmit, control } = useForm<IFormInput>({
     defaultValues: {
-      credential: "",
+      username: "",
       password: "",
     },
   });
 
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    apiCall<LoginResponse>({
-      url: "/api/v1/auth/login",
-      data,
-      callback: (res) => {
-        const token = res.data.token;
-        Cookies.set(LocalNames.token, token, {
-          expires: makeDateFromNow(
-            import.meta.env.VITE_TOKEN_EXPIRATION_SECOND
-          ),
-        });
-        navigate("/");
-      },
-      method: "post",
-    });
+  const onSubmit: SubmitHandler<IFormInput> = ({ username, password }) => {
+    mutate({ username, password });
+  };
+
+  const handleCheckFunction = (e: React.ChangeEvent<HTMLInputElement>) => {
+    rememberMeSet(e.target.checked);
   };
 
   //  "credential": "admin@satrin.com",
@@ -50,22 +79,89 @@ const Login = () => {
   }
 
   return (
-    <Box component="form" onSubmit={handleSubmit(onSubmit)}>
-      <Controller
-        name="credential"
-        control={control}
-        render={({ field }) => (
-          <TextField {...field} variant="outlined" label="نام کاربری" />
-        )}
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row-reverse",
+        backgroundColor: "lightgray",
+      }}
+    >
+      <img
+        style={{ height: "100vh", width: "50%", objectFit: "cover" }}
+        src={LoginImage}
       />
-      <Controller
-        name="password"
-        control={control}
-        render={({ field }) => (
-          <TextField {...field} variant="outlined" label="کلمه عبور" />
-        )}
-      />
-      <Button type="submit">ورود</Button>
+
+      <Box sx={{ display: "flex", justifyContent: "center", flexGrow: 1 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "#FFFFFF",
+            gap: 4,
+            p: 4,
+            borderRadius: 4,
+            width: "360px",
+          }}
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {/* <HoldingImage /> */}
+          </Box>
+          <Controller
+            name="username"
+            control={control}
+            render={({ field: { ref, ...rest } }) => (
+              <Input
+                {...rest}
+                variant="outlined"
+                label="یوزرنیم یا ایمیل"
+                inputRef={ref}
+                autoComplete="on"
+              />
+            )}
+          />
+
+          <Controller
+            name="password"
+            control={control}
+            render={({ field: { ref, ...rest } }) => (
+              <Input
+                {...rest}
+                variant="outlined"
+                label="کلمه عبور"
+                type="password"
+                inputRef={ref}
+                autoComplete="on"
+              />
+            )}
+          />
+
+          <FormControlLabel
+            control={
+              <Checkbox
+                sx={{ padding: 0, ml: 1 }}
+                onChange={handleCheckFunction}
+                checked={rememberMe}
+                size="small"
+              />
+            }
+            label="مرا به خاطر بسپار"
+            sx={{ mr: 0 }}
+          />
+          <Button type="submit">وارد شوید</Button>
+          <Typography sx={{ textAlign: "center" }} className="cursor-pointer">
+            فراموشی رمز عبور
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   );
 };
